2b91dad7f1af097248da468a01364fc192238204
“jeferson” <jefersonsatoshi@hotmail.com>
2025-05-18 04:11:56 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -20,8 +20,11 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
+	public Category() {
+	}
+
+	
 	public Category(Long id, String name) {
-		super();
 		this.id = id;
 		this.name = name;
 	}
