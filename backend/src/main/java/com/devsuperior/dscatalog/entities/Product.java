b0030321181720e55f247e48b7832cec98a06409
JefersonSatoshi <jefersonsatoshi@hotmail.com>
2025-07-27 21:57:39 -0300

    Update Product entity and UserInsertDTO

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.devsuperior.dscatalog.projections.IdProjection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,7 +20,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable {
+public class Product implements Serializable, IdProjection<Long> {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -54,6 +56,7 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
+	@Override
 	public Long getId() {
 		return id;
 	}
